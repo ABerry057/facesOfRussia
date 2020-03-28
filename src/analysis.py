7e0c4a585f58a27383ca9127ff318145f794c439
ABerry057 <alexander_berry@brown.edu>
2020-03-28 19:11:21 -0400

    working topic modeling

--- src/analysis.py
+++ src/analysis.py
@@ -1,10 +1,21 @@
 """
 Performs MALLET-based LDA topic modeling from generated corpus.
 """
-
-from gensim.test.utils import common_corpus, common_dictionary
+import pickle
 from gensim.models.wrappers import LdaMallet
+from pathlib import Path
 
-path_to_mallet_binary = "/home/alex/mallet-2.0.8/mallet-2.0.8/bin/mallet"
-model = LdaMallet(path_to_mallet_binary, corpus=common_corpus, num_topics=20, id2word=common_dictionary)
-vector = model[common_corpus[0]]  # LDA topics of a documents
+parent_dir = Path(__file__).parent.parent
+seed = 19
+
+dictionary, bow_corpus = pickle.load(open(f'{parent_dir}/data/corpus.pickle', 'rb'))
+path_to_mallet_binary = f'{parent_dir}/src//mallet-2.0.8/bin/mallet'
+
+model = LdaMallet(path_to_mallet_binary,
+                  corpus=bow_corpus,
+                  num_topics=20,
+                  id2word=dictionary,
+                  iterations=5000,
+                  random_seed=seed)
+
+vector = model[bow_corpus[0]]  # LDA topics of a documents
--- src/genCorpus.py
+++ src/genCorpus.py
@@ -2,22 +2,35 @@
 Generates MALLET format corpus from raw texts.
 """
 import os
+import pickle
 from tqdm import tqdm
 from gensim import corpora
-from gensim.utils import simple_preprocess, lemmatize
+from gensim.utils import simple_preprocess
 
 import nltk
 # nltk.download('stopwords')  # run once
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
+from nltk.stem import WordNetLemmatizer
 from pathlib import Path
 
 parent_dir = Path(__file__).parent.parent
 txt_dir = f'{parent_dir}/data/raw_texts'
 
-p_text_list = []
+lemmatizer = WordNetLemmatizer()
+
+pro_text_list = []
 for file in tqdm(os.listdir(txt_dir), desc="Load and preprocess"):
     with open(f'{txt_dir}/{file}', 'r') as raw:
-        text = lemmatize(raw.read(), stopwords=frozenset(stop_words))
+        text = raw.read()
     text_list = simple_preprocess(text)
-    p_text_list += text_list
+    lemma_list = [lemmatizer.lemmatize(word) for word in text_list]
+    stopped_list = [word for word in lemma_list if word not in stop_words]
+    pro_text_list += stopped_list
+pro_text_list = [pro_text_list]
+# create dictionary from preprocessed text
+dictionary = corpora.Dictionary(pro_text_list)
+bow_corpus = [dictionary.doc2bow(text) for text in pro_text_list]
+mallet_pack = (dictionary, bow_corpus)
+# save BoW corpus as pickle
+pickle.dump(mallet_pack, open(f'{parent_dir}/data/corpus.pickle', "wb" ) )
